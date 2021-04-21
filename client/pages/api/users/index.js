260768f2893af8b18a9cc857acc345371888498b
Louisa <louisawong95@hotmail.com>
2021-04-21 16:20:45 -0400

    error: fixed empty GET

--- client/pages/api/users/index.js
+++ client/pages/api/users/index.js
@@ -6,12 +6,12 @@ dbConnect();
 export default async (req,res) => {
     const {method} = req;
 
-    switch(method) {
-        case 'GET':
-            try {
-              const notes = await User.findOne({id: })
-            }catch (err) {
+    // switch(method) {
+    //     case 'GET':
+    //         try {
+    //           const notes = await User.findOne({id: })
+    //         }catch (err) {
 
-            }
+    //         }
     }
 }
