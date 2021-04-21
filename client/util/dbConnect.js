1c9843f799df1210a763fe4544ae76a08d192b87
Louisa <louisawong95@hotmail.com>
2021-04-21 11:48:16 -0400

    trying to connect mongoose atlas

--- client/util/dbConnect.js
+++ client/util/dbConnect.js
@@ -1,5 +1,7 @@
 import mongoose from 'mongoose';
 
+const databaseURI = process.env.MONGO_URI || "mongodb+srv://general:oJtmLp85RvK3PbEo@plantiful.xmjku.mongodb.net/PlantifulDB?retryWrites=true&w=majority"
+
 const connection = {};
 
 async function dbConnect () {
