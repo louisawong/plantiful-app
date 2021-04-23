ccec12d895f2a3b33f707af31a46c7b5fb20f374
Louisa <louisawong95@hotmail.com>
2021-04-23 12:09:02 -0400

    feat: added fontstyle to global

--- client/pages/index.js
+++ client/pages/index.js
@@ -5,7 +5,7 @@ import style from '../styles/Home.module.scss'
 import {useAuth} from '../firebase/auth';
 import Link from 'next/link'
 import React, {useEffect,useState} from 'react'
-import Navigation from '../components/Navigation/Navigation';
+import Navigation from '../components/navigation/navigation';
 
 export default function Home() {
 
@@ -16,6 +16,8 @@ export default function Home() {
       <Head>
         <title>Plantiful</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="preconnect" href="https://fonts.gstatic.com"/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet"/>
       </Head>
       <main>
         <Navigation/>
